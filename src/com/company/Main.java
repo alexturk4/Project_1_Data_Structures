a4f06f2e2e228b723cad96fe8ead077dca4dfa8e
Alex Turk <alexturk4@gmail.com>
2019-09-28 17:20:00 -0500

    header comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,25 @@
+// Anesh Turk Project #1
+// Data Structures and Algorithms CS 3345
+// Fall 2019 - Due 9/29/19
+
+/*
+    The purpose of this project is to compare the efficiency of the following 6 sorting algorithms:
+        1. Insertion Sort
+        2. Selection Sort
+        3. Quick Sort
+        4. Merge Sort
+        5. Heap Sort
+        6. Radix Sort
+
+    On arrays which already exist in the following states:
+        1. InOrder (ascending)
+        2. ReverseOrder (descending)
+        3. AlmostOrder (80% sorted 20% unsorted)
+        4. Random
+
+    We will be keeping track of the number of comparisons, element movements, and the running time and will then compare
+        the results between them
+ */
 package com.company;
 
 public class Main {
